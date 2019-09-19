317736cc8410ec35981cf42113783035ebe75265
Morten Olsen <morten222@hotmail.com>
2019-09-19 21:35:08 +0200

    TC01: Test for getDescription() added (RED BAR)

--- test/fleet/StatusTest.java
+++ test/fleet/StatusTest.java
@@ -1,7 +1,18 @@
 package fleet;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatusTest {
+    @Test
+    public void testStatus() {
+        Status status1 = Status.DOCKEDATHOME;
+        assertEquals("Docked at home", status1.getDescription());
+        Status status2 = Status.DOCKEDAWAY;
+        assertEquals("Docked away", status2.getDescription());
+        Status status3 = Status.ONVOYAGE;
+        assertEquals("On voyage...", status3.getDescription());
 
+    }
 }
