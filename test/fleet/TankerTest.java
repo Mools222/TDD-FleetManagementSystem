22490d7e57b8ac1453b5a6419ea359fefc0e6494
Morten Olsen <morten222@hotmail.com>
2019-09-19 22:40:19 +0200

    TC10: Test for EVERYTHING add

--- test/fleet/TankerTest.java
+++ test/fleet/TankerTest.java
@@ -2,9 +2,21 @@ package fleet;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.Month;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class TankerTest {
     @Test
     public void testCreateTanker1() {
+        Tanker tanker = new Tanker();
+        assertEquals("Seawise Giant", tanker.getName());
+        assertEquals(LocalDate.of(1979, Month.DECEMBER, 4), tanker.getLaunchDate());
+        assertEquals(564763, tanker.getDeadweightTonnage());
+        assertEquals(673380, tanker.getTonnageVolume());
+        assertEquals("empty", tanker.getCargoDescription());
+        assertEquals(Status.DOCKED_AT_HOME, tanker.getStatus());
 
     }
 
