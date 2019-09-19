ecbcdc609037abb56adf9478303e543f7d5acba5
Morten Olsen <morten222@hotmail.com>
2019-09-19 21:44:11 +0200

    TC02: Test for getLaunchDate() passed (GREEN BAR)

--- src/fleet/ContainerShip.java
+++ src/fleet/ContainerShip.java
@@ -1,8 +1,15 @@
 package fleet;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 public class ContainerShip {
 
     public String getName() {
         return "Marco Polo";
+    }
+
+    public LocalDate getLaunchDate() {
+        return LocalDate.of(2012, Month.NOVEMBER, 6);
     }
 }
