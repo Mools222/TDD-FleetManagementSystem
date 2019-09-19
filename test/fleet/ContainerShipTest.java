233d761e38f7e6f29ba89b1ba00abab411ba1a18
Morten Olsen <morten222@hotmail.com>
2019-09-19 21:55:58 +0200

    TC03: Test for getContainerCount() add

--- test/fleet/ContainerShipTest.java
+++ test/fleet/ContainerShipTest.java
@@ -25,5 +25,6 @@ class ContainerShipTest {
         assertEquals("Hansa Carrier", containership.getName());
         assertEquals(LocalDate.of(1989, Month.FEBRUARY, 1), containership.getLaunchDate());
         assertEquals(26366, containership.getDeadweightTonnage());
+        assertEquals(1799, containership.getContainerCount());
     }
 }
