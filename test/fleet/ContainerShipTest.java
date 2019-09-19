48903fc654d16a1dba5f791b925698c373357fc0
Morten Olsen <morten222@hotmail.com>
2019-09-19 21:41:36 +0200

    TC02: Test for getName() added (RED BAR)

--- test/fleet/ContainerShipTest.java
+++ test/fleet/ContainerShipTest.java
@@ -7,7 +7,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class ContainerShipTest {
     @Test
     public void testCreateContainerShip1() {
-
+        ContainerShip containership = new ContainerShip();
+        assertEquals("Marco Polo", containership.getName());
     }
 
 }
