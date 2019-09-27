93513289479767230b4c06de6c13e98746a66851
Morten Olsen <morten222@hotmail.com>
2019-09-27 19:32:13 +0200

    TC19: testCreateFleet2() added TC19: Test for getNumberOfFreighters() add

--- test/fleet/FleetTest.java
+++ test/fleet/FleetTest.java
@@ -2,6 +2,9 @@ package fleet;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FleetTest {
@@ -10,6 +13,15 @@ class FleetTest {
         Fleet fleet = new Fleet();
         assertEquals(0, fleet.getNumberOfFreighters());
         assertEquals(0, fleet.getTotalDeadweightTonnage());
+    }
+
+    @Test
+    public void testCreateFleet2() {
+        ContainerShip containership = new ContainerShip("Hansa Carrier", "empty", LocalDate.of(1989, Month.FEBRUARY, 1), 26366, Status.DOCKED_AT_HOME, 1799);
+        Tanker tanker = new Tanker("Exxon Valdez", "empty", LocalDate.of(1986, Month.OCTOBER, 14), 214861, Status.DOCKED_AT_HOME, 235000);
+        Fleet fleet = new Fleet(containership, tanker);
+
+        assertEquals(2, fleet.getNumberOfFreighters());
 
     }
 }
